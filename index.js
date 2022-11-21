eadcda6b49c097e0930f37b7bcde96babba339f2
nitrr26 <nitrr26@gmail.com>
2022-11-21 21:03:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+
+// const app = express();
+
+// app.get("/", (req, res) =>{
+//     res.send("hello from server");
+// }).listen(3000);
+// console.log("listening in port 3000");
